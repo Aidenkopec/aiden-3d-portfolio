38428b5fc73e93d921a2c525c5982e9c922d6d78
Aidenkopec <97480728+Aidenkopec@users.noreply.github.com>
2023-11-29 12:44:13 -0700

    Adding new color theme and background image.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,12 +5,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
-        "black-100": "#100d25",
-        "black-200": "#090325",
-        "white-100": "#f3f3f3",
+        primary: "#0a192f",
+        secondary: "#a8b2d1",
+        tertiary: "#2c3f5a",
+        "black-100": "#102a43",
+        "black-200": "#011627",
+        "white-100": "#e6f1ff",
       },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        "hero-pattern": "url('/src/assets/herobg_midnight_blue.png')",
       },
     },
   },

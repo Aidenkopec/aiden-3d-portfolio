8f19ac9c03a5c0790615517ba0b720f9255dd757
Aidenkopec <97480728+Aidenkopec@users.noreply.github.com>
2023-11-29 19:47:20 -0700

    About me summary updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,34 +47,32 @@ const About = () => {
                 variants={fadeIn("", "", 0.1, 1)}
                 className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
             >
-                I am a dedicated software developer with a diverse range of experience in full-stack development,
-                front-end & back-end development. As a Freelance Web Developer, I design, develop, and maintain robust,
-                scalable applications using Node.js, Express.js, and MongoDB, and integrate various APIs. I craft
-                engaging user interfaces using React, JavaScript, HTML5/CSS3, Framer Motion, and Three.js and leverage
-                AWS for reliable cloud infrastructure. I also develop websites using Wix and WordPress, demonstrating
-                versatility and proficiency in different web development platforms.
+                I'm a passionate software developer with expertise in full-stack, front-end, and back-end development.
+                I've built robust applications using Node.js, Express.js, MongoDB, and APIs, while also creating
+                engaging user interfaces with React, JavaScript, and HTML5/CSS3. My experience at One Piece IT led to
+                significant performance improvements for web and mobile applications. At Launchcode, I developed a
+                crucial quote-to-cash system for the Oil & Gas industry, showcasing my technical and project management
+                skills.
+
                 <br/>
                 <br/>
-                In my role at SAIT Polytechnic and Launchcode, I spearheaded the development of a quote-to-cash system
-                for commercial clients in the Oil & Gas industry, managing front-end and back-end development, client
-                requirements gathering, and workflow design. I've worked extensively with React, Typescript, Node.js,
-                PostgreSQL, Git, Docker, and AWS.
+                In terms of technology, I've honed my skills at the Southern Alberta Institute of Technology, covering a
+                wide range of languages and databases, including Java, C++, Python, JavaScript, TypeScript, React,
+                AngularJS, and various databases like PostgreSQL, MySQL, NoSQL, and MongoDB. I'm also well-versed in
+                development tools and environments like IntelliJ IDEA, MS Visual Studio, Slack, Git, GitHub, GitLab,
+                AWS, and Docker.
                 <br/>
                 <br/>
-                My educational background includes a Diploma in Software Development from the Southern Alberta Institute
-                of Technology, where I honed my technical skills in Java, C++, Python, JavaScript, TypeScript, React,
-                Node.js, Express.js, AngularJS, Java Web, JSON, PostgreSQL, MySQL, NoSQL, MongoDB, HTML, and CSS. I am
-                proficient with development tools and environments such as IDE (IntelliJ IDEA, MS Visual Studio), Slack
-                Board, Git, GitHub, GitLab, AWS, and Docker.
+                My project portfolio features notable creations such as an AI Image Generator and a 3D T-Shirt Designer,
+                demonstrating my proficiency in cutting-edge web development. I've also developed an Article Summarizer,
+                showcasing my skills in creating concise article summaries
                 <br/>
                 <br/>
-                I am a diligent, detail-oriented problem solver with strong communication and leadership skills, honed
-                through my experience as a hockey referee and team leader in software projects. I have completed several
-                impressive projects, including an AI Image Generation Application, a modern 3D T-Shirt Designer, a
-                Multi-data-sorting Algorithm, and a YouTube Clone Application. In my free time, I enjoy skiing, hiking,
-                and officiating hockey games, which has taught me the importance of teamwork, communication, and
-                decision-making under pressure. I believe my diverse set of technical skills, soft skills, and passion
-                for software development makes me a strong candidate for your team.
+                Outside of tech, I enjoy outdoor activities and sports like skiing, hiking, and fitness, which offer
+                both physical and mental challenges. My passion for hockey, as both a player and referee, has taught me
+                valuable skills such as quick decision-making, teamwork, and resilience under pressure. These
+                experiences have significantly influenced my professional approach, enriching my skill set and
+                perspectives.
             </motion.p>
 
 

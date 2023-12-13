4919b7743febacbd0710313cdfe76d9129573636
Aidenkopec <97480728+Aidenkopec@users.noreply.github.com>
2023-12-13 13:18:27 -0700

    Updating about me statement

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,32 +47,32 @@ const About = () => {
                 variants={fadeIn("", "", 0.1, 1)}
                 className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
             >
-                I'm a passionate software developer with expertise in full-stack, front-end, and back-end development.
-                I've built robust applications using Node.js, Express.js, MongoDB, and APIs, while also creating
-                engaging user interfaces with React, JavaScript, and HTML5/CSS3. My experience at One Piece IT led to
-                significant performance improvements for web and mobile applications. At Launchcode, I developed a
-                crucial quote-to-cash system for the Oil & Gas industry, showcasing my technical and project management
-                skills.
+                As a software developer, I bring a distinctive mix of expertise in full-stack, front-end, and back-end
+                development. My journey at One Piece IT was marked by turning web and mobile applications into highly
+                efficient systems. At Launchcode, I led the development of a key quote-to-cash system for the Oil & Gas
+                industry, blending my technical skills with effective project management.
 
                 <br/>
                 <br/>
-                In terms of technology, I've honed my skills at the Southern Alberta Institute of Technology, covering a
-                wide range of languages and databases, including Java, C++, Python, JavaScript, TypeScript, React,
-                AngularJS, and various databases like PostgreSQL, MySQL, NoSQL, and MongoDB. I'm also well-versed in
-                development tools and environments like IntelliJ IDEA, MS Visual Studio, Slack, Git, GitHub, GitLab,
-                AWS, and Docker.
+                My educational background at the Southern Alberta Institute of Technology equipped me with a broad skill
+                set, including Java, C++, Python, JavaScript, TypeScript, React, and various databases.
+                Beyond programming languages, I've developed innovative projects like an AI Image Generator and a 3D
+                T-Shirt Designer, demonstrating my capability in advanced web technologies. Additionally, my work on an
+                Article Summarizer using the GPT API underscores my ability to simplify complex information processing.
                 <br/>
                 <br/>
-                My project portfolio features notable creations such as an AI Image Generator and a 3D T-Shirt Designer,
-                demonstrating my proficiency in cutting-edge web development. I've also developed an Article Summarizer,
-                showcasing my skills in creating concise article summaries.
+                Outside of technology, I pursue outdoor activities like skiing, hiking, and fitness, which contribute to
+                my mental agility and physical well-being. My engagement in hockey, as a player and referee, has been
+                instrumental in developing skills like quick decision-making and teamwork. These experiences have not
+                only shaped my personal life but also positively influenced my professional approach, adding depth to my
+                technical expertise.
                 <br/>
-                <br/>
-                Outside of tech, I enjoy outdoor activities and sports like skiing, hiking, and fitness, which offer
-                both physical and mental challenges. My passion for hockey, as both a player and referee, has taught me
-                valuable skills such as quick decision-making, teamwork, and resilience under pressure. These
-                experiences have significantly influenced my professional approach, enriching my skill set and
-                perspectives.
+                {/*<br/>*/}
+                {/*Outside of tech, I enjoy outdoor activities and sports like skiing, hiking, and fitness, which offer*/}
+                {/*both physical and mental challenges. My passion for hockey, as both a player and referee, has taught me*/}
+                {/*valuable skills such as quick decision-making, teamwork, and resilience under pressure. These*/}
+                {/*experiences have significantly influenced my professional approach, enriching my skill set and*/}
+                {/*perspectives.*/}
             </motion.p>
 
 
